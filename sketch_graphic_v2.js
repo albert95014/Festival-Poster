f57d6e697b5edf99d1654af27b088ab9f0b9f9f7
albert95014 <albertyang.me@gmail.com>
2022-06-10 00:25:54 -0700

    poster update and UI changes

--- sketch_graphic_v2.js
+++ sketch_graphic_v2.js
@@ -41,13 +41,15 @@ function draw() {
   festivalDate = document.getElementById("date").value.toUpperCase();
   festivalLoc = document.getElementById("location").value.toUpperCase();
   separator = document.getElementById("separator").value;
+  artistTextSize = Number(document.getElementById("artistTextSize").value);
+  // console.log(artistTextSize);
 
   //Text Options
-  textSize(64); //should equal topBillingSize
+  textSize(artistTextSize); //should equal topBillingSize
   festivalNameSize = 96;
-  topBillingSize = 64;
-  midBillingSize = 40;
-  btmBillingSize = 24;
+  topBillingSize = artistTextSize;
+  midBillingSize = artistTextSize;
+  btmBillingSize = artistTextSize;
   lineHeight = textSize();
   textFont(font);
   textAlign(CENTER);
@@ -62,7 +64,7 @@ function draw() {
     }
 
 
-    for (var y=0; y<6; y++) {
+    for (var y=0; y<5; y++) {
       drawBackground(random(width), random(height), color(random(255), random(255), random(255)))
     }
 
@@ -110,18 +112,29 @@ function draw() {
         textSize(btmBillingSize)
       }
 
-      // fill(255);
+      // fill(200);
       drawText(lineup[x], xLoc, yLoc);
       yLoc += lineHeight;
     }
 
-    //drawFestivalName(festivalName, btmPin, btmArcAmp, topPin, topArcAmp)
+    //drawFestivalName(festivalName, btmPin, btmArcAmp, topPin, topArcAmp, wiggleLvl)
     //  btmPin = set lowest position of arc text
     //  btmArcAmp = amplitude of the upward bend (on bottom)
     //  topPin = set highest position of arc text
     //  topArcAmp = amplitude of the downward bend (on top)
-    drawFestivalInfo(festivalName, 24, 0, 250, 150);
-    drawFestivalInfo(festivalDate + separator + festivalLoc, height - 80, 0, height - 36, 0);
+    //  wiggleLvl = how wiggly the text is (from 0 to ~5)
+    push();
+      // rotate(random(-0.05, 0.05));
+      for (var i=0; i<8; i++) {
+          drawFestivalInfo(festivalName, 28, 0, 160, i*10, random(3));
+          // drawFestivalInfo(festivalDate + separator + festivalLoc, height - 120, i*8, height - 36, 0, 0.4);
+      }
+    pop();
+    // drawFestivalInfo(festivalDate + separator + festivalLoc, height - 80, 0, height - 36, 0);
+    for (var i=0; i<4; i++) {
+        drawFestivalInfo(festivalDate + separator + festivalLoc, height - 120, i*12, height - 32, 0, 0.6);
+    }
+
   }
 
   started = false;
@@ -133,9 +146,6 @@ function draw() {
                 0, 0, myPoster.width, myPoster.height);
   clear();
 
-  // resizeCanvas(800, 800, true);
-
-
   image(myPoster,
         (width - myPoster.width)/2, 24,
         myPoster.width, myPoster.height);
@@ -146,9 +156,42 @@ function draw() {
 //ORIGINAL SIMPLE TEXT
 function drawText(artist) {
   push();
-    noStroke();
-    fill(255);
-    text(artist, xLoc, yLoc);
+    // noStroke();
+    // fill(255);
+
+    stroke(255);
+    strokeWeight(1);
+
+    let points = font.textToPoints(artist, xLoc, yLoc, textSize(), {
+          sampleFactor: 0.8,
+          simplifyThreshold: 0
+        });
+
+    let artistTextWidth = textWidth(artist);
+    let counter = random(10);
+
+      beginShape();
+    for (var i=points.length-1; i>0; i--) {
+      points[i].x = points[i].x + 0.5*sin(counter);
+      points[i].y = points[i].y - 0.5*sin(counter);
+      counter++;
+      ellipse(points[i].x - artistTextWidth/2, points[i].y, 0.5);
+
+      // let pointDistance = dist(points[i].x, points[i].y, points[i-1].x, points[i-1].y);
+      //
+      //
+      //   if (pointDistance > 3) {
+      //     endShape(CLOSE);
+      //     beginShape();
+      //   }
+      //     vertex(points[i].x  - artistTextWidth/2, points[i].y);
+
+      //subtract artistTextWidth/2 to center the text
+      //ellipse(points[i].x - artistTextWidth/2, points[i].y, 0.5);
+    }
+    endShape(CLOSE);
+
+    // text(artist, xLoc, yLoc);
   pop();
 }
 
@@ -167,45 +210,67 @@ function drawBackground(x, y, bgColor) {
   }
 }
 
-function drawFestivalInfo(festivalName, btmPin, btmArcAmp, topPin, topArcAmp){
+function drawFestivalInfo(festivalName, btmPin, btmArcAmp, topPin, topArcAmp, wiggleLvl){
     points = font.textToPoints(festivalName, xLoc, yLoc, festivalNameSize, {
-       sampleFactor: 0.8,
+       sampleFactor: 0.9,
        simplifyThreshold: 0
       });
     bounds = font.textBounds(festivalName, xLoc, yLoc, festivalNameSize)
 
     push();
-      // fill(255);
-      noFill();
-      stroke(255);
-      strokeWeight(2);
+      fill(255, 150);
+      noStroke();
+      blendMode(SOFT_LIGHT);
+      // noFill();
+      // stroke(255, 240);
+      // strokeWeight(2);
+
+      let counter = random(10);
+
+      //Method for cutting out contours/counters in letters (thank you!)
+      //https://github.com/spacetypeco/generative-typography-SP22/blob/main/tutorials/glitching.md
+      let drawingContour = false;
 
       beginShape();
-        for (var i=1; i<points.length; i++) {
+        for (let pt of points) {
 
           //last two parameters are left and right of festival name box
-          points[i].x = map(points[i].x,
-                            xLoc, xLoc + bounds.w,
-                            20, width - 40)
+          pt.x = map(pt.x, xLoc, xLoc + bounds.w, 20, width - 40)
 
           //yMapDest = map(points[i].x, 20, width-40, -100, 100)
-          yMapDest = map(points[i].x, 20, width-40, 0, PI)
+          yMapDest = map(pt.x, 20, width-40, 0, PI)
 
           //last two parameters are top and bottom of festival name box
-          points[i].y = map(points[i].y,
-                            yLoc-bounds.h, yLoc,
-                            btmPin + btmArcAmp*sin(yMapDest), topPin - topArcAmp*sin(yMapDest));
+          pt.y = map(pt.y, yLoc-bounds.h, yLoc,
+                     btmPin + btmArcAmp*sin(yMapDest), topPin - topArcAmp*sin(yMapDest));
 
-          distance = dist(points[i].x, points[i].y,
-                          points[i-1].x, points[i-1].y)
+          pt.x = pt.x + wiggleLvl*sin(counter);
+          pt.y = pt.y - wiggleLvl*sin(counter);
+          counter++;
+        }
 
-          if (distance > 5) {
-            endShape(CLOSE);
-            beginShape();
+        let prevPt = points[0];
+
+        for (let pt of points) {
+          distance = dist(pt.x, pt.y,
+                          prevPt.x, prevPt.y)
+
+          if (distance > 16) {
+            if (drawingContour) {
+              endContour();
+            }
+            drawingContour = true;
+            beginContour();
           }
 
-          vertex(points[i].x, points[i].y);
+          vertex(pt.x, pt.y);
+          prevPt = pt;
         }
+
+      if (drawingContour) {
+        endContour();
+      }
+
       endShape(CLOSE);
     pop();
 }
